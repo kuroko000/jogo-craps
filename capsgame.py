6761dda45dd49da7b1da759e06a8ab3fd82321f4
kuroko000 <121641551+kuroko000@users.noreply.github.com>
2024-04-17 13:50:56 -0300

    Create capsgame.py

---
+++ capsgame.py
@@ -0,0 +1,13 @@
+import random
+
+#dado entre 2 a 12
+
+numero_aleatorio = random.randint (2,12)
+
+# 7 ou 11 pontuação natural e você ganha
+
+
+
+if numero_aleatorio == (7 or 11):
+    print ("Você venceu")
+    
