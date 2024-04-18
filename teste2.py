979b59b2fcd1cbac5182222180f5adc5eb952d63
kuroko000 <121641551+kuroko000@users.noreply.github.com>
2024-04-18 11:00:37 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,32 +1,32 @@
 import random #importação para podermos simular um dado
 
-def roll_dice(): #def que vai atribuir o valor as jogadas do game
+def rolaguem_de_dados(): #def que vai atribuir o valor as jogadas do game
     return random.randint(1, 6) + random.randint(1, 6)
 
-def craps_game(): #
-    point = 0
-    while True: #loop do gamep, pra fazer os teste do valor recebido na def roll_dice
+def game(): 
+    ponto = 0
+    while True: #loop do game, pra fazer os teste do valor recebido na def rolaguem_de_dados
         input("Pressione Enter para lançar os dados...")
-        dice_sum = roll_dice()
-        print("Você lançou um total de", dice_sum)
+        resultado = rolaguem_de_dados()
+        print("Você lançou um total de", resultado)
         
-        if point == 0: #primeira rodada
-            if dice_sum in [7, 11]:
+        if ponto == 0: #primeira rodada
+            if resultado in [7, 11]:
                 print("Você tirou um natural! Você ganhou!")
                 break
-            elif dice_sum in [2, 3, 12]:
+            elif resultado in [2, 3, 12]:
                 print("Você tirou um craps! Você perdeu!")
                 break
             else:
-                print("Seu ponto é", dice_sum)
-                point = dice_sum
+                print("Seu ponto é", resultado)
+                ponto = resultado
         else:
-            if dice_sum == point: #segunda rodada em diante
+            if resultado == ponto: #segunda rodada em diante
                 print("Você tirou seu ponto novamente! Você ganhou!")
                 break
-            elif dice_sum == 7:
+            elif resultado == 7:
                 print("Você tirou um 7! Você perdeu!")
                 break
 
 print("Bem-vindo ao jogo de Craps!")
-craps_game()
+game()
