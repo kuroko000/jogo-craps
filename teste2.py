4b6438057c9db454c739e19660809aad0b8d7d70
kuroko000 <121641551+kuroko000@users.noreply.github.com>
2024-04-18 09:48:18 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,16 +1,16 @@
-import random
+import random #importação para podermos simular um dado
 
-def roll_dice():
+def roll_dice(): #def que vai atribuir o valor as jogadas do game
     return random.randint(1, 6) + random.randint(1, 6)
 
-def craps_game():
+def craps_game(): #
     point = 0
-    while True:
+    while True: #loop do gamep, pra fazer os teste do valor recebido na def roll_dice
         input("Pressione Enter para lançar os dados...")
         dice_sum = roll_dice()
         print("Você lançou um total de", dice_sum)
         
-        if point == 0:
+        if point == 0: #primeira rodada
             if dice_sum in [7, 11]:
                 print("Você tirou um natural! Você ganhou!")
                 break
@@ -21,7 +21,7 @@ def craps_game():
                 print("Seu ponto é", dice_sum)
                 point = dice_sum
         else:
-            if dice_sum == point:
+            if dice_sum == point: #segunda rodada em diante
                 print("Você tirou seu ponto novamente! Você ganhou!")
                 break
             elif dice_sum == 7:
